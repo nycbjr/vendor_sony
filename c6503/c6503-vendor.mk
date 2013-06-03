f3e5ab00598569e0116eaabf4054b5883a831915
nycbjr <nycbjr@gmail.com>
2013-06-02 20:39:52 -0400

    initial push

---
+++ c6503/c6503-vendor.mk
@@ -0,0 +1,5 @@
+PRODUCT_PLATFORM := Lagan
+$(call inherit-product, $(LOCAL_PATH)/c6503-partial.mk)
+$(call inherit-product, vendor/qcom/lagan/lagan-partial.mk)
+$(call inherit-product, vendor/sony/lagan/lagan-partial.mk)
+
---
+++ c6603/c6603-vendor.mk
@@ -0,0 +1,5 @@
+PRODUCT_PLATFORM := Lagan
+$(call inherit-product, $(LOCAL_PATH)/c6603-partial.mk)
+$(call inherit-product, vendor/sony/lagan/lagan-partial.mk)
+$(call inherit-product-if-exists, vendor/qcom/lagan/lagan-partial.mk)
+
